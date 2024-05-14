37d3f4b9eab85e3180ac1668bce3194a7ae6909e
Tigran <tiko.bag@mail.ru>
2024-05-14 19:05:29 +0300

    feat: рефакторинг параметра

--- src/main/java/ru/practicum/shareit/item/dao/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/dao/CommentRepository.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByItem_id(long ItemId);
+    List<Comment> findByItem_id(long itemId);
 
     List<Comment> findByItem_Owner_id(long userId);
 }
